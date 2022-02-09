da051dd6c71929353d97d579f59985fd8d551f71
Andrii <www.andriy12@gmail.com>
2022-02-09 18:46:49 +0200

    organized dynamic creation of cards with the help of axios and adjusted the work of choosing the type of dough and pizza sizes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React,{useState ,useEffect} from "react";
 import { Routes, Route } from "react-router-dom";
 import {Header} from './components'
 import {Home, Cart} from "./components/pages";
+import axios from "axios";
 
 
 function App() {
+
+  const [pizzas, setPizzas] = useState([])
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json')
+      .then(({data}) => {
+        setPizzas(data.pizzas)
+      })
+  },[])
+
   return (
     
     <div className="wrapper">
     <Header/>
       <div className="content">
       <Routes>
-        <Route path="/" exact element={<Home/>}/>
+        <Route path="/" exact element={<Home items={pizzas}/>}/>
         <Route path="/cart" exact element={<Cart/>}/> 
       </Routes>
       </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from "./header/header";
 import Categories from "./categories/categories";
 import Button from "./button/button";
 import SortPopap from "./sortPopap/sortPopap";
+import PizzaBlock from "./pizzaBlock/pizzaBlock";
 
-export {Header, Categories, Button , SortPopap}
+export {Header, Categories, Button , SortPopap, PizzaBlock}
